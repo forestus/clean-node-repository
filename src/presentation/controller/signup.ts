d13d4410d089e5072ffc072858f4c2d846fdfdfc
forestus <guilhermepm164@gmail.com>
2021-05-25 17:09:44 -0300

    refactor: correct controller name SignUpController

--- src/presentation/controller/signup.ts
+++ src/presentation/controller/signup.ts
@@ -2,7 +2,7 @@ import { HttpRequest, HttpResponse } from '../protocols/http'
 import { Controller } from '../protocols/controller'
 import { MissingParamError } from '../errors/missing-param-error'
 import { badRequest } from '../helpers/http-helper'
-export class SignUpControlle implements Controller {
+export class SignUpController implements Controller {
   handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredFields) {
