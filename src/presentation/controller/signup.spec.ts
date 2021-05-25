d2ac34021a1cafb36981adc750046a05907810ca
forestus <guilhermepm164@gmail.com>
2021-05-25 16:24:48 -0300

    refactor: ensure SignUpcontroller.handle implement correct interfaces

--- src/presentation/controller/signup.spec.ts
+++ src/presentation/controller/signup.spec.ts
@@ -1,32 +1,32 @@
-import {SignUpController} from "./signup"
+import { SignUpController } from './signup'
 describe('SignUp Controller', () => {
-    test('should return 400 if no name is provided', () => {
-        const sut = new SignUpController();
-        const httpRequest = {
-            body:{
-                email: 'forestus7@gmail.com',
-                password: 'sugoi777',
-                passwordConfirmation: 'sugoi777'
-            }
-        }
-        const httpResponse = sut.handle(httpRequest);
-        expect(httpResponse.statusCode).toBe(400);
-        //to be compara também o ponteiro dos objetos
-        //toequal compara os valores apenas
-        expect(httpResponse.body).toEqual(new Error('Missing param: name'));
-    });
+  test('should return 400 if no name is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        email: 'forestus7@gmail.com',
+        password: 'sugoi777',
+        passwordConfirmation: 'sugoi777'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    // to be compara também o ponteiro dos objetos
+    // toequal compara os valores apenas
+    expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+  })
 
-    test('should return 400 if no name is provided', () => {
-        const sut = new SignUpController();
-        const httpRequest = {
-            body:{
-                name: 'forestus7',
-                password: 'sugoi777',
-                passwordConfirmation: 'sugoi777'
-            }
-        }
-        const httpResponse = sut.handle(httpRequest);
-        expect(httpResponse.statusCode).toBe(400); 
-        expect(httpResponse.body).toEqual(new Error('Missing param: email'));
-    });
-});
+  test('should return 400 if no name is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'forestus7',
+        password: 'sugoi777',
+        passwordConfirmation: 'sugoi777'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
+  })
+})
