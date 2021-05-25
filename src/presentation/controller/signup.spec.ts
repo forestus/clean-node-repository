a8226997c68f644d411249e4533eac0ea1e207f4
forestus <guilhermepm164@gmail.com>
2021-05-25 16:05:36 -0300

    test: ensure SignUpControler return an error 400 if no email is provided

--- src/presentation/controller/signup.spec.ts
+++ src/presentation/controller/signup.spec.ts
@@ -15,4 +15,18 @@ describe('SignUp Controller', () => {
         //toequal compara os valores apenas
         expect(httpResponse.body).toEqual(new Error('Missing param: name'));
     });
+
+    test('should return 400 if no name is provided', () => {
+        const sut = new SignUpController();
+        const httpRequest = {
+            body:{
+                name: 'forestus7',
+                password: 'sugoi777',
+                passwordConfirmation: 'sugoi777'
+            }
+        }
+        const httpResponse = sut.handle(httpRequest);
+        expect(httpResponse.statusCode).toBe(400); 
+        expect(httpResponse.body).toEqual(new Error('Missing param: email'));
+    });
 });
