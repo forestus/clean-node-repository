601e36cba4d6646c9efa4e247ef6918bd620de31
forestus <guilhermepm164@gmail.com>
2021-05-25 16:56:43 -0300

    refactor: simplify code

--- src/presentation/controller/signup.ts
+++ src/presentation/controller/signup.ts
@@ -6,8 +6,12 @@ export class SignUpController {
     if (!httpRequest.body.name) {
       return badRequest(new MissingParamError('name'))
     }
-    if (!httpRequest.body.email) {
-      return badRequest(new MissingParamError('email'))
+
+    const requiredFields = ['name', 'email']
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field))
+      }
     }
   }
 }
